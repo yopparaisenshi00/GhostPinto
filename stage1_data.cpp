e1ef3d35750cb2ebbf6555718b765fc52bb8e92f
krorom <krorom3710@gmail.com>
2018-06-22 17:35:10 +0900

    StageObj修正

--- stage1_data.cpp
+++ stage1_data.cpp
@@ -57,19 +57,20 @@ STAGE_DATA stage1_time[] = {
 STAGE_DATA stage1_kill[] = {
 	//全21匹
 	//         出現     関数		座標			速度			加速度    最大速度		Z値
-	STAGE_DATA(15,		zMove,		V2(1260,762),	V2(1.0f, 1.0f), V2(0,-1), V2(0,-2),	rand_PINTO),
-	STAGE_DATA(15,		zMove,		V2(660, 762),	V2(1.0f, 1.0f), V2(0,-1), V2(0,-2),	rand_PINTO),
+
 	STAGE_DATA(15,		zMove,		V2(660, 512),	V2(1.0f, 1.0f), V2(0,-1), V2(0,-2),	rand_PINTO),
+	STAGE_DATA(15,		zMove,		V2(660, 762),	V2(1.0f, 1.0f), V2(0,-1), V2(0,-2),	rand_PINTO),
 	STAGE_DATA(15,		zMove,		V2(1260,512),	V2(1.0f, 1.0f), V2(0,-1), V2(0,-2),	rand_PINTO),
+	STAGE_DATA(15,		zMove,		V2(1260,762),	V2(1.0f, 1.0f), V2(0,-1), V2(0,-2),	rand_PINTO),
 
 	STAGE_DATA(20,		Tombo,		V2(700, 452),	V2(0.3f, 0.3f), V2(0,-1), V2(0,-2),	rand_PINTO),
 	STAGE_DATA(20,		Tombo,		V2(800, 452),	V2(0.3f, 0.3f), V2(0,-1), V2(0,-2),	rand_PINTO),
 	STAGE_DATA(20,		Tombo,		V2(1120,452),	V2(0.3f, 0.3f), V2(0,-1), V2(0,-2),	rand_PINTO),
 	STAGE_DATA(20,		Tombo,		V2(1220,452),	V2(0.3f, 0.3f), V2(0,-1), V2(0,-2),	rand_PINTO),
 
+	STAGE_DATA(30,		Big,		V2(480, 512),	V2(0.7f, 0.7f), V2(0,-1), V2(0,-2),	rand_PINTO),
 	STAGE_DATA(30,		Big,		V2(960, 512),	V2(0.7f, 0.7f), V2(0,-1), V2(0,-2),	rand_PINTO),
 	STAGE_DATA(30,		Big,		V2(1440,512),	V2(0.7f, 0.7f), V2(0,-1), V2(0,-2),	rand_PINTO),
-	STAGE_DATA(30,		Big,		V2(480, 512),	V2(0.7f, 0.7f), V2(0,-1), V2(0,-2),	rand_PINTO),
 
 	STAGE_DATA(30,		Normal,		V2(600, 452),	V2(1.5f, 1.5f), V2(0,-1), V2(0,-2),	rand_PINTO),
 	STAGE_DATA(30,		Normal,		V2(700, 372),	V2(1.5f, 1.5f), V2(0,-1), V2(0,-2),	rand_PINTO),
@@ -91,13 +92,6 @@ STAGE_DATA stage1_kill[] = {
 
 LAND_SCAPE_DATA stage1_bg_effect[] = {
 
-	{ LSD::BOT	 ,0,BG_Mini_capsule_l,V2(1000,1024),V2(0,0),V2(0,0),V2(0,0),0 },
-
-	{ LSD::BOT	 ,0,BG_Mini_capsule_d,V2(1160,1024),V2(0,0),V2(0,0),V2(0,0),0 },
-
-	{ LSD::BOT	 ,0,BG_Break_capsule_u,V2(1360,1024),V2(0,0),V2(0,0),V2(0,0),0 },
-	{ LSD::BOT	 ,0,BG_Display_b,V2(1300,800),V2(0,0),V2(0,0),V2(0,0),0 },
-
 	{ LSD::BOT	 ,0,BG_Capsule_l,V2(200,1024),V2(0,0),V2(0,0),V2(0,0),10 },
 	{ LSD::BOT	 ,0,BG_Display_a,V2(128,900),V2(0,0),V2(0,0),V2(0,0),0 },
 
@@ -106,6 +100,29 @@ LAND_SCAPE_DATA stage1_bg_effect[] = {
 
 	{ LSD::BOT	 ,0,BG_Capsule_l,V2(600,1024),V2(0,0),V2(0,0),V2(0,0),10 },
 	{ LSD::BOT	 ,0,BG_Display_c,V2(664,900),V2(0,0),V2(0,0),V2(0,0),0 },
+
+	{ LSD::BOT	 ,0,BG_Mini_capsule_l,V2(1000,1024),V2(0,0),V2(0,0),V2(0,0),10 },
+
+	{ LSD::BOT	 ,0,BG_Mini_capsule_d,V2(1160,1024),V2(0,0),V2(0,0),V2(0,0),10 },
+
+	{ LSD::BOT	 ,0,BG_Break_capsule_u,V2(1360,1024),V2(0,0),V2(0,0),V2(0,0),10 },
+	{ LSD::BOT	 ,0,BG_Display_b,V2(1300,800),V2(0,0),V2(0,0),V2(0,0),0 },
+
+	{ LSD::BOT	 ,0,BG_container,V2(1850,1024),V2(0,0),V2(0,0),V2(0,0), 0 },
+	{ LSD::BOT	 ,0,BG_pc,V2(770,1024),V2(0,0),V2(0,0),V2(0,0), 0 },
+
+
+	{ LSD::CENTER,0,BG_Fly_mini_capsule_l,V2(200,412),V2(0,0),V2(0,0),V2(0,0),20 },
+
+	{ LSD::CENTER,0,BG_Fly_mini_capsule_d,V2(500,612),V2(0,0),V2(0,0),V2(0,0),20 },
+
+	{ LSD::CENTER,0,BG_Fly_mini_capsule_m,V2(800,412),V2(0,0),V2(0,0),V2(0,0),20 },
+
+	{ LSD::CENTER,0,BG_Fly_mini_capsule_d,V2(1500,512),V2(0,0),V2(0,0),V2(0,0),20 },
+
+	{ LSD::CENTER,0,BG_Fly_mini_capsule_d,V2(1800,612),V2(0,0),V2(0,0),V2(0,0),20 },
+
+
 
 	{ LSD::BOT	 ,0,BG_Capsule_l,V2(1200,1024),V2(0,0),V2(0,0),V2(0,0),40 },
 	{ LSD::BOT	 ,0,BG_Display_a,V2(1128,700),V2(0,0),V2(0,0),V2(0,0),40 },
@@ -119,32 +136,19 @@ LAND_SCAPE_DATA stage1_bg_effect[] = {
 	{ LSD::BOT	 ,0,BG_Mini_capsule_d,V2(260,1024),V2(0,0),V2(0,0),V2(0,0),40 },
 	{ LSD::BOT	 ,0,BG_Mini_capsule_d,V2(560,1024),V2(0,0),V2(0,0),V2(0,0),40 },
 
+	{ LSD::BOT	 ,0,BG_Fly_capsule_m,V2(2050,400),V2(0,0),V2(0,0),V2(0,0),40 },
+	{ LSD::BOT	 ,0,BG_Display_b,V2(2000,400),V2(0,0),V2(0,0),V2(0,0),40 },
 
-	{ LSD::CENTER,0,BG_Fly_mini_capsule_l,V2(200,312),V2(0,0),V2(0,0),V2(0,0),20 },
-
-	{ LSD::CENTER,0,BG_Fly_mini_capsule_d,V2(500,512),V2(0,0),V2(0,0),V2(0,0),20 },
-
-	{ LSD::CENTER,0,BG_Fly_mini_capsule_d,V2(1500,412),V2(0,0),V2(0,0),V2(0,0),20 },
-
-	{ LSD::CENTER,0,BG_Fly_mini_capsule_d,V2(1800,512),V2(0,0),V2(0,0),V2(0,0),20 },
-
-	{ LSD::BOT	 ,0,BG_Fly_capsule_m,V2(2150,200),V2(0,0),V2(0,0),V2(0,0),40 },
-	{ LSD::BOT	 ,0,BG_Display_b,V2(2100,200),V2(0,0),V2(0,0),V2(0,0),40 },
-
-	{ LSD::CENTER,0,BG_Fly_mini_capsule_m,V2(800,312),V2(0,0),V2(0,0),V2(0,0),20 },
-
-
-	{ LSD::CENTER,0,BG_Fly_capsule_d,V2(-1200,312),V2(0,0),V2(0,0),V2(0,0),60 },
-	{ LSD::CENTER,0,BG_Fly_capsule_l,V2(-400,312),V2(0,0),V2(0,0),V2(0,0),60 },
-	{ LSD::CENTER,0,BG_Fly_capsule_d,V2(400,312),V2(0,0),V2(0,0),V2(0,0),60 },
-	{ LSD::CENTER,0,BG_Fly_capsule_l,V2(1200,312),V2(0,0),V2(0,0),V2(0,0),60 },
-	{ LSD::CENTER,0,BG_Fly_capsule_d,V2(2000,312),V2(0,0),V2(0,0),V2(0,0),60 },
-	{ LSD::CENTER,0,BG_Fly_capsule_d,V2(2800,312),V2(0,0),V2(0,0),V2(0,0),60 },
-
-
-	{ LSD::BOT	 ,0,BG_container,V2(1850,1024),V2(0,0),V2(0,0),V2(0,0), 0 },
-	{ LSD::BOT	 ,0,BG_pc,V2(770,1024),V2(0,0),V2(0,0),V2(0,0), 0 },
 	{ LSD::BOT	 ,0,BG_pc,V2(1400,1024),V2(0,0),V2(0,0),V2(0,0), 40 },
+
+
+	{ LSD::CENTER,0,BG_Fly_capsule_d,V2(0,612),V2(0,0),V2(0,0),V2(0,0),60 },
+	{ LSD::CENTER,0,BG_Fly_capsule_l,V2(400,612),V2(0,0),V2(0,0),V2(0,0),60 },
+	{ LSD::CENTER,0,BG_Fly_capsule_d,V2(800,612),V2(0,0),V2(0,0),V2(0,0),60 },
+	{ LSD::CENTER,0,BG_Fly_capsule_l,V2(1200,612),V2(0,0),V2(0,0),V2(0,0),60 },
+	{ LSD::CENTER,0,BG_Fly_capsule_d,V2(1600,612),V2(0,0),V2(0,0),V2(0,0),60 },
+	{ LSD::CENTER,0,BG_Fly_capsule_d,V2(2000,612),V2(0,0),V2(0,0),V2(0,0),60 },
+
 
 	{ LSD::CENTER,0,BG_Fly_capsule_l,V2(1200,312),V2(0,0),V2(0,0),V2(0,0),-60 },
 
