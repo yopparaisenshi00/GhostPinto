777b3796091d8afbafed7a408200d8193f3181fb
yopparaisenshi00 <kochan0810@icloud.com>
2018-06-29 04:20:31 +0900

    カウントダウン４秒

--- sceneMain.cpp
+++ sceneMain.cpp
@@ -178,7 +178,7 @@ void	sceneMain::Update()
 		IEX_StopSound(BGM_TITLE);
 		IEX_PlaySound(BGM_MAIN, FALSE); //BGM
 
-		//count_down = 2;
+		count_down = 4;
 		count_down_timer = 0;
 		//-------------------------------------------------------------------
 		timer = 80 * 60;
